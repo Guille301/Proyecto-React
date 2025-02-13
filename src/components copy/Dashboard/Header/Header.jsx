ca605cb958bff2665fac7b15f22c22c4d14bbb0d
Guille301 <guillermogau300@gmail.com>
2025-02-12 21:44:19 -0300

    Cambio estetico en header

--- src/components copy/Dashboard/Header/Header.jsx
+++ src/components copy/Dashboard/Header/Header.jsx
@@ -1,6 +1,5 @@
 import "./Header.css";
-import logo from "./logo.svg";
-
+import logo from "./logo.jpg";
 const Header = ({ onLogout }) => {
 
   const _onHandleClick =  () => {
@@ -12,7 +11,7 @@ const Header = ({ onLogout }) => {
       <div className="col-12 d-flex justify-content-between align-items-center my-3">
         <div className="d-flex align-items-center">
           <img src={logo} alt="Logo" className="mr-2" width={70} />
-          <h1>@ To-Do App</h1>
+          <h1>Gestion de ejercicios</h1>
         </div>
         <div>
           <button type="submit" 
